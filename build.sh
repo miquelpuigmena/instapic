d2f9265fa1c6692f847fc5192662220864e7e4fd
miquelpuigmena <miquelpuigmena@gmail.com>
2020-11-26 14:25:31 +0700

    Automation Fix: args from docker build 2 dockerfile

--- build.sh
+++ build.sh
@@ -4,29 +4,35 @@ if [[ $# -eq 0 || $# -gt 1 ]]
     exit 0
 fi
 
-if [[ $1 ==  "--test-api" ]]; 
-  then
+if [[ $1 ==  "--test-api" ]]
+then
+    set -a && source instapic-frontend/src/.env && source instaPic-backend/.env && docker-compose down
     docker-compose build
     docker-compose run --rm api npm test
-elif [[ $1 == "--test-web" ]];
-  then
+    docker-compose down
+elif [[ $1 == "--test-web" ]]
+then
+    set -a && source instapic-frontend/src/.test.env && source instaPic-backend/.env && docker-compose down
     docker-compose build
     docker-compose run --rm web npm test
-elif [[ $1 == "--test-all" ]];
-  then
+    docker-compose down
+elif [[ $1 == "--test-all" ]]
+then
+    set -a && source instapic-frontend/src/.test.env && source instaPic-backend/.env && docker-compose down
     docker-compose build
     docker-compose run --rm api npm test
     docker-compose run --rm web npm test
-elif [[ $1 == "--run" ]];
-  then
+    docker-compose down
+elif [[ $1 == "--run" ]]
+then
+    set -a && source instapic-frontend/src/.env && source instaPic-backend/.env && docker-compose down
     docker-compose build  
-    set -a && source instapic-frontend/.env && source instaPic-backend/.env && docker-compose down
-    set -a && source instapic-frontend/.env && source instaPic-backend/.env && docker-compose up
-elif [[ $1 == "--clean" ]];
-  then
-    set -a && source instapic-frontend/.env && source instaPic-backend/.env && docker-compose down
+    MODE=PROD docker-compose up
+elif [[ $1 == "--clean" ]]
+then
+    set -a && source instapic-frontend/src/.env && source instaPic-backend/.env && docker-compose down
 else
    echo "Args accepted are --tests-api, --test-web --test-all, --run or --clean"
-   exit 0
+   exit 1
 fi
 exit 0
